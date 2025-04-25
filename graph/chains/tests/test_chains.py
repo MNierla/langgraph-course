d12e3cfebd3607c857ba9ca119633dae5bc3eb75
Eden Marco <emarco177@gmail.com>
2025-04-25 14:08:21 +0300

    10-self-RAG merged into own branch

--- graph/chains/tests/test_chains.py
+++ graph/chains/tests/test_chains.py
@@ -10,6 +10,7 @@ from typing import cast
 from graph.chains.retrieval_grader import GradeDocuments, retrieval_grader
 from graph.chains.generation import generation_chain
 from ingestion import retriever
+from graph.chains.hallucination_grader import hallucination_grader, GradeHallucinations
 
 
 def test_retrival_grader_answer_yes() -> None:
@@ -67,5 +68,30 @@ def test_generation_chain() -> None:
     assert len(docs) == 4
 
     generation = generation_chain.invoke({"context": docs, "question": question})
+
     # no real criterion here; testcase is meant as sanity-check
     pprint(generation)
+
+def test_hallucination_grader_answer_yes() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+
+    generation = generation_chain.invoke({"context": docs, "question": question})
+    res: GradeHallucinations = hallucination_grader.invoke(
+        {"documents": docs, "generation": generation}
+    )
+    assert res.binary_score
+
+
+def test_hallucination_grader_answer_no() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+
+    res: GradeHallucinations = hallucination_grader.invoke(
+        {
+            "documents": docs,
+            "generation": "In order to make pizza we need to first start with the dough",
+        }
+    )
+    assert not res.binary_score
+
