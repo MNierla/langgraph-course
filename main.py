b2cea0aa5dfa2ecd1a6c74b7581c3010146c9ac5
mnierla <michaelnierla@web.de>
2026-05-24 21:05:29 +0200

    Added outline / goal as comment

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# First Aim - Setup a C-RAG (corrective rag)
+# > extend "normal" RAG by a reflection process
+# > reflection process shall overwatch if selected chunks from the RAG-database actually are suitable
+# > if reflection process (grade_document) deems chunks as unsufficient, do websearch
+
 from dotenv import load_dotenv
 
 load_dotenv()
