795e7a26de075c59fa7879b6e9572b0ffa7b7cd5
mnierla <michaelnierla@web.de>
2026-05-24 17:26:51 +0200

    Resolved merge conflicts and added more comments and notes.

--- chains.py
+++ chains.py
@@ -76,11 +76,13 @@ revise_instructions = """Revise your previous answer using the new information.
     - You should use the previous critique to remove superfluous information from your answer and make SURE it is not more than 250 words.
 """
 
+# reuse the llm as revisor but in revisor mmode just allow the "ReviseAnser" tool
+# > as we reuse the actor_prompt_template, we have to change first_instruction to the revise_instructions
 revisor = actor_prompt_template.partial(
     first_instruction=revise_instructions
 ) | llm.bind_tools(tools=[ReviseAnswer], tool_choice="ReviseAnswer")
 
-
+# NOTE: in this revision we don't use the revisor yet
 if __name__ == "__main__":
     human_message = HumanMessage(
         content="Write about AI-Powered SOC / autonomous soc  problem domain,"
--- schemas.py
+++ schemas.py
@@ -18,6 +18,7 @@ class AnswerQuestion(BaseModel):
     )
 
 
+# NOTE: ReviseAnswer is an extension of AnswerQuestion!
 class ReviseAnswer(AnswerQuestion):
     """Revise your original answer to your question."""
 
