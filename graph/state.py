aabafe86a2963c9dc744f9aaafe35cfe8090de16
mnierla <michaelnierla@web.de>
2026-05-24 22:40:21 +0200

    Added notes from video.

--- graph/state.py
+++ graph/state.py
@@ -1,6 +1,8 @@
 from typing import List, TypedDict
 
-
+# NOTE:
+# Compared to previous reflection/reflexion-agents we don't use MessageGraph-nodes for this project
+# MessageGraph-nodes can just handle "messages"-list but we need more attributes
 class GraphState(TypedDict):
     """
     Represents the state of our graph.
@@ -15,4 +17,5 @@ class GraphState(TypedDict):
     question: str
     generation: str
     web_search: bool
+    # documents contains documents from vector-storage and/or web_search
     documents: List[str]
