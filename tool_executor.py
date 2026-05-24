e8cd5d5188ec50e21256a2b50c3105ff1a65d4d3
mnierla <michaelnierla@web.de>
2026-05-24 17:48:12 +0200

    Added notes to tool_executor.py

--- tool_executor.py
+++ tool_executor.py
@@ -4,10 +4,20 @@ load_dotenv()
 
 from langchain_core.tools import StructuredTool
 from langchain_tavily import TavilySearch
+# Notes on ToolNode
+# (class) ToolNode
+# A node that runs the tools called in the last AIMessage.
+# It can be used either in StateGraph with a "messages" state key (or a custom key passed via ToolNode's 'messages_key'). If multiple tool calls are requested, they will be run in parallel. The output will be a list of ToolMessages, one for each tool call.
+# Tool calls can also be passed directly as a list of ToolCall dicts.
+# > basically toolnode looks into the latest ai-message in messages for tool calls
+# > if a tool call exists, it will be executed
 from langgraph.prebuilt import ToolNode
 
 from schemas import AnswerQuestion, ReviseAnswer
 
+# tavily_tool is already a useable tool which we could bind to an llm, but we do it differently here
+# > we use tavily_tool in two StructuredTools below to distinguish between AnswerQuestion and ReviseAnswer
+# > what is this good for? It gives clearer output in the trace!
 tavily_tool = TavilySearch(max_results=5)
 
 
