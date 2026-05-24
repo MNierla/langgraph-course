014aca68880356f9bb527a43491a80aeb4f7d373
mnierla <michaelnierla@web.de>
2026-05-24 22:46:17 +0200

    Added only notes.

--- graph/nodes/retrieve.py
+++ graph/nodes/retrieve.py
@@ -3,10 +3,13 @@ from typing import Any, Dict
 from graph.state import GraphState
 from ingestion import retriever
 
-
+# NOTE:
+# I guess that question is passed alongside with documents to make it accessible by the follow-up parts of the graph
+# In the video it is said, that returning the question is optional here. It was added for curiosity reasons.
 def retrieve(state: GraphState) -> Dict[str, Any]:
     print("---RETRIEVE---")
     question = state["question"]
 
+    # retriever.invoke does semantic search
     documents = retriever.invoke(question)
     return {"documents": documents, "question": question}
