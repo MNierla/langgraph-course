cba3d68ac7970db64e774ae6ba8782ddc59a6f5c
mnierla <michaelnierla@web.de>
2026-05-25 09:03:18 +0200

    note for upcoming self-rag

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # NOTE:
-# First Aim - Setup a C-RAG (corrective rag)
+# First Step - Setup a C-RAG (corrective rag)
 # > extend "normal" RAG by a reflection process
 # > reflection process shall overwatch if selected chunks from the RAG-database actually are suitable
 # > if reflection process (grade_document) deems chunks as unsufficient, do websearch
+#
+# Second Step - Self-Reflection
+# > extend C-RAG by additional checks of the generated answer 
+# > this shall avoid hallucinations and off-topic answers
 
 from dotenv import load_dotenv
 
