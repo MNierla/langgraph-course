c97ae4bbbb3543e8eb71b654ef720f6ab2e40fb3
mnierla <michaelnierla@web.de>
2026-05-25 11:51:50 +0200

    Added explicit cast to AIMessage

--- nodes.py
+++ nodes.py
@@ -1,4 +1,6 @@
 from dotenv import load_dotenv
+from typing import cast
+from langchain_core.messages import AIMessage
 from langgraph.graph import MessagesState
 from langgraph.prebuilt import ToolNode
 
@@ -14,7 +16,12 @@ def run_agent_reasoning(state: MessagesState) -> MessagesState:
     """
     Run the agent reasoning node.
     """
-    response = llm.invoke([{"role": "system", "content": SYSYEM_MESSAGE}, *state["messages"]])
+    # note that the llm here is imported from react where tools got bound to it
+    # cast added as well as llm only returns BaseMessages which are too generic for AnyMessage which are required by MessagesState
+    response = cast(AIMessage,llm.invoke([{"role": "system", "content": SYSYEM_MESSAGE}, *state["messages"]]))
     return {"messages": [response]}
 
+# Remember ToolNode
+# > it checks whether the last message of the dialogue is an AI-message
+# > if it is an AI-message and it contains a tool-call, then it will be executed by the ToolNode
 tool_node = ToolNode(tools)
